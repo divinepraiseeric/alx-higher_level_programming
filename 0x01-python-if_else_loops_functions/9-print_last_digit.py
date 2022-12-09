57d54f287479caa46d3ec881007e955343526590
DivinePraise Eric <praisechinemerem@gmail.com>
2022-12-08 16:58:15 -0800

    9-print_last_digit.py 2

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    print("{}".format(abs(int(number)) % 10))
+    print("{}".format(abs(int(number)) % 10), end="")
     return abs(int(number)) % 10
