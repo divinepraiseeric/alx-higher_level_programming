12cb1b3fe81646af3c3b84e5314799171ac02d28
DivinePraise Eric <praisechinemerem@gmail.com>
2022-12-08 17:18:13 -0800

    12-fizzbuzz.py 1

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,7 +3,7 @@ def fizzbuzz():
     for i in range(101):
         if i % 15 == 0:
             i = "FizzBuzz"
-        if i % 3 == 0:
+        elif i % 3 == 0:
             i = "Fizz"
         elif i % 5 == 0:
             i = "Buzz"
